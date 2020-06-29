e3da6b268ef7cdd7f4fe0d0fb956fffcf3e6d87a
Rashi Bhansali <52106856+rashi-bhansali@users.noreply.github.com>
2020-06-29 23:08:31 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,12 +8,15 @@ app = Flask(__name__)
 @app.route("/")
 
 def build_model():
+    '''
     connection = MongoClient('mongodb+srv://hrlanes-mongodb-reader:hrlanes%401234@hrlanes-production-i5mve.mongodb.net', 27017)
     db = connection['hrlanes-web-db']
     data = db['users']
     ex = data.find({"$and": [{'ProfileSummaryInfo': {"$exists": True}}, {'recommenderProcessed': {"$exists": True}}, {'recommenderProcessed': True }]})
     helper = Helper()
     d = helper.createDictionary(ex)
+    '''
+    d = {(5,3): [(27, "abcd jfbskgb")], (16,3): [(21, "hgfjaefv jbhjesjgh"), (10, "jrbvawhfvawbkjw jkbfj")]}
     resumeList = []
     for key in d:
         if len(d[key])>0:  # check if resumes/details exist 
