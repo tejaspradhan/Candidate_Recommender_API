1b10291925d8d526a16aa84a4b04a4ce50792f4a
Rashi Bhansali <52106856+rashi-bhansali@users.noreply.github.com>
2020-06-29 23:50:13 +0530

    Update application.py
    Checking if mongo db cluster is getting connected

--- application.py
+++ application.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 
 def build_model():
-    '''
     connection = MongoClient('mongodb+srv://hrlanes-mongodb-reader:hrlanes%401234@hrlanes-production-i5mve.mongodb.net', 27017)
     db = connection['hrlanes-web-db']
     data = db['users']
     ex = data.find({"$and": [{'ProfileSummaryInfo': {"$exists": True}}, {'recommenderProcessed': {"$exists": True}}, {'recommenderProcessed': True }]})
+    '''
     helper = Helper()
     d = helper.createDictionary(ex)
     d = {(5,3): [(27, "abcd jfbskgb")], (16,3): [(21, "hgfjaefv jbhjesjgh"), (10, "jrbvawhfvawbkjw jkbfj")]}
