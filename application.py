05d0e7c233bc80c58d26048b865dc6606363b209
Tejas Pradhan <pradhan.tejas135@gmail.com>
2020-07-07 14:58:52 +0530

    Update application.py

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ def build_model():
                 for f in resumeList:
                     documents.append(helper.cleanTextAndTokenize(f))
                     helper.create_tfidf(str(key), documents, doc_included)
-        return json.dumps(d)
+        return str(d)
     
     elif method == 'recommend':
         exp = request.args.get('e')
